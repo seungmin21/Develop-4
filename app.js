282c2e9422936d88d8ae90f00c56a58d3b91fa34
ohseungmin <ohmin2886@gmail.com>
2023-12-07 10:59:56 +0900

    devString의 콘솔 확인 결과 1(문자열) 타입 string을 확인할 수 있다.

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ var devArray = [1, 2, 3];
 var devObject = {name:"dev", age:20};
 
 console.log(devNumber, typeof(devNumber)); // 1(숫자열) number
+console.log(devString, typeof(devString)); // 1(문자열) string
