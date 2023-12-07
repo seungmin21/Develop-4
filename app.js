4eda8dedbb26eec9bc5168465f2d4204e893223e
ohseungmin <ohmin2886@gmail.com>
2023-12-07 10:58:44 +0900

    devNumber의 콘솔 확인 결과 1(숫자열) 타입 number를 확인할 수 있다.

---
+++ app.js
@@ -0,0 +1,7 @@
+var devNumber = 1;
+var devString = "1";
+var devBoolean = true;
+var devArray = [1, 2, 3];
+var devObject = {name:"dev", age:20};
+
+console.log(devNumber, typeof(devNumber)); // 1(숫자열) number
